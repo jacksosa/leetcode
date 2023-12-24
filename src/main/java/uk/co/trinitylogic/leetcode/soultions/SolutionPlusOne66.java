858b01f86cea038f158d556abd3f168f97b4cfb9
Samuel Jackson <sam1_jackson@hotmail.com>
2023-12-24 09:41:36 +0000

    66. Plus One

--- src/main/java/uk/co/trinitylogic/leetcode/soultions/SolutionPlusOne66.java
+++ src/main/java/uk/co/trinitylogic/leetcode/soultions/SolutionPlusOne66.java
@@ -4,6 +4,10 @@ public class SolutionPlusOne66 {
 
     // https://leetcode.com/problems/plus-one/
 
+    // if the last number is less then nine we can just increment this and return,
+    // otherwise its a rollover to the array grows by one and contains 1 and the rest zeros
+    // ie. [9,9,9,9,9] + 1 = [1,0,0,0,0,0] - ints have a default value of 0 so we only have to set the value at index 0
+
     public int[] plusOne(int[] digits) {
 
         for (int i = digits.length - 1; i >= 0; i--) {
