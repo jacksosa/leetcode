66447a5cffcaee9f746d763e32724da3df2b4744
Samuel Jackson <sam1_jackson@hotmail.com>
2023-12-25 16:04:48 +0000

    125. Valid Palindrome

---
+++ src/main/java/uk/co/trinitylogic/leetcode/soultions/SolutionValidPalindrome.java
@@ -0,0 +1,16 @@
+package uk.co.trinitylogic.leetcode.soultions;
+
+public class SolutionValidPalindrome {
+
+    // https://leetcode.com/problems/valid-palindrome/
+
+    public boolean isPalindrome(String s) {
+        String c = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase().strip();
+        StringBuilder r = new StringBuilder();
+        for (int i = c.length() - 1; i >= 0; i--) {
+            char val = c.charAt(i);
+            r.append(val);
+        }
+        return c.contentEquals(r);
+    }
+}
---
+++ src/test/java/uk/co/trinitylogic/leetcode/soultions/SolutionValidPalindromeTest.java
@@ -0,0 +1,48 @@
+package uk.co.trinitylogic.leetcode.soultions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionValidPalindromeTest {
+
+    @Test
+    void isPalindrome_Example_1() {
+        String input = "A man, a plan, a canal: Panama";
+        boolean output = true;
+        // Explanation: "amanaplanacanalpanama" is a palindrome.
+        SolutionValidPalindrome target = new SolutionValidPalindrome();
+        boolean result = target.isPalindrome(input);
+        assertEquals(output, result);
+    }
+
+    @Test
+    void isPalindrome_Example_2() {
+        String input = "race a car";
+        boolean output = false;
+        // Explanation: "raceacar" is not a palindrome.
+        SolutionValidPalindrome target = new SolutionValidPalindrome();
+        boolean result = target.isPalindrome(input);
+        assertEquals(output, result);
+    }
+
+    @Test
+    void isPalindrome_Example_3() {
+        String input = " ";
+        boolean output = true;
+        // Explanation: s is an empty string "" after removing non-alphanumeric characters.
+        // Since an empty string reads the same forward and backward, it is a palindrome.
+        SolutionValidPalindrome target = new SolutionValidPalindrome();
+        boolean result = target.isPalindrome(input);
+        assertEquals(output, result);
+    }
+
+    @Test
+    void isPalindrome_Example_4() {
+        String input = "0P";
+        boolean output = false;
+        SolutionValidPalindrome target = new SolutionValidPalindrome();
+        boolean result = target.isPalindrome(input);
+        assertEquals(output, result);
+    }
+}
