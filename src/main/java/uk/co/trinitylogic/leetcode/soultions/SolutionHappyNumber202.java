58bff24f4ba2c507fb2b5c087c00228760982492
Samuel Jackson <sam1_jackson@hotmail.com>
2024-01-02 09:45:43 +0000

    202. Happy Number - Cleanup

--- src/main/java/uk/co/trinitylogic/leetcode/soultions/SolutionHappyNumber202.java
+++ src/main/java/uk/co/trinitylogic/leetcode/soultions/SolutionHappyNumber202.java
@@ -5,18 +5,14 @@ public class SolutionHappyNumber202 {
     // https://leetcode.com/problems/happy-number/
 
     public boolean isHappy(int n) {
-
-        String s = Integer.toString(n);
-        int result = 0;
         int count = 0;
 
-        while (result != 1) {
-            int total = 0;
-            for (int c : s.toCharArray()) {
-                total = total + (int) Math.pow(Character.getNumericValue(c), 2);
+        while (n != 1) {
+            int sub = 0;
+            for (int c : Integer.toString(n).toCharArray()) {
+                sub = sub + (int) Math.pow(Character.getNumericValue(c), 2);
             }
-            s = Integer.toString(total);
-            result = total;
+            n = sub;
             count++;
             if (count > 10) {
                 return false;
